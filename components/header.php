b17a60e81cb0da581542859d806b391fa978a3bc
Markus Hylleberg <markushylleberg@gmail.com>
2019-11-21 16:24:49 +0100

    style.css and header.php

--- components/header.php
+++ components/header.php
@@ -12,23 +12,25 @@
 </head>
 <body>
 <!-- Header -->
-<div id="headerContainer">
-    <div id="logoWrapper">
-        Logo her 
+<div id="headerContainer" class="fixed row p-1">
+    <div id="logoWrapper" class="col-6 align-center">
+        <p>Limeless</p> 
         <!-- <img src="" alt="" id="logo"> -->
     </div>
-    <div id="navigationWrapper">
-        <ul id="mainMenu" class="hide-on-mobile">
+    <div id="navigationWrapper" class="col-6 row">
+        <ul id="mainMenu" class="hide-on-mobile small-padding text-right col-10 sm-col-2">
             <li class="menu-item">
-                <a href="">Menu item 1</a>
+                <a href="">About</a>
             </li>
-            <li class="menu-item">
-                <a href="">Menu item 2</a>
+            <li class="menu-item mx-1">
+                <a href="">How to use</a>
             </li>
         </ul>
-        <div class="avatar">avatar</div>
-        <div id="burger" class="hide-on-desktop">burger icon</div>
-        <div id="mobileMenuPanel">
+        <div class="avatar text-right col-2 sm-col-10">
+            <img src="src/images/test-user.png" class="round-img small-avatar mx-1">
+        </div>
+        <div id="burger" class="hide-on-desktop align-center"><i class="fa fa-bars text-white"></i></div>
+        <div id="mobileMenuPanel" class="hide-on-desktop hidden">
             <div class="inner">
                 <ul id="mobileMenu">
                     <li class="menu-item">
